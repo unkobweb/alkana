6a29fb2970dcd6b80bc61cf2b31f21235cf22f71
Alexandre Sieg <sieg.alexandre@gmail.com>
2021-11-11 15:56:11 +0100

    Put the project on git

---
+++ index.js
@@ -0,0 +1,70 @@
+const { Client, Intents } = require('discord.js');
+const { exec } = require("child_process");
+require('dotenv').config();
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient();
+
+const client = new Client({ 
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES
+  ] 
+});
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', async (message) => {
+  if (message.content.charAt(0) == "!") {
+    
+    const command = message.content.slice(1);
+    const args = command.split(" ");
+    args.shift();
+
+    switch (command) {
+      case "whitelist":
+        if (args.length == 1){
+          const player_name = args[0]
+          exec(`screen -S alkana -p 0 -X stuff "whitelist add ${player_name}^M"`,async (err, stdout, stderr) => {
+            if (err || stderr) {
+              message.channel.send(`❌ ${player_name} n'a pas pu être ajouté à la whitelist`)
+              await prisma.whitelist.create({
+                data: {
+                  minecraft_name: player_name,
+                  discord_id: message.author.id,
+                  discord_username: message.author.username,
+                  success: false
+                }
+              })
+            } else {
+              exec(`screen -S alkana -p 0 -X stuff "whitelist reload^M"`,(err, stdout, stderr) => {
+                if (err || stderr){
+                  message.channel.send(`❌ La whitelist n'a pas pu être rafraichir`)
+                } else {
+                  //exec(`screen -S alkana -p 0 -X stuff say ${player_name} a été ajouté à la whitelist par ${message.author.username}^M`)
+                  message.channel.send(`✔️ ${player_name} peut se connecter sur Alkana !`)
+                }
+              })
+              await prisma.whitelist.create({
+                data: {
+                  minecraft_name: player_name,
+                  discord_id: message.author.id,
+                  discord_username: message.author.username,
+                  success: true
+                }
+              })
+            }
+          })
+        } 
+        break;
+    
+      default:
+        break;
+    }
+  }
+});
+
+client.login('OTA3Njg3MjExNzYyMzMxNzI4.YYqzlw.WJ-yd9BndfY_aA_vhU5Wp4jp7eQ');
